d47e9265cb21b34a99237e6b8bc7f9d12fdb794a
NGUfromPB <99163430+NGUfromPB@users.noreply.github.com>
2023-02-23 14:35:33 +0300

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -15,71 +15,24 @@ public class UserServiceImpl implements UserService {
     Util util = new Util();
 
     public void createUsersTable() {
-        String craft = "CREATE TABLE IF NOT EXISTS USERS" +
-                "  id       BIGINT       PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                "  name     VARCHAR(250) DEFAULT NULL," +
-                "  lastname VARCHAR(250) DEFAULT NULL," +
-                "  age      TINYINT      DEFAULT NULL)";
-        try (Statement statement =  util.getConnection().createStatement()) {
-            statement.executeUpdate(craft);
-        } catch (SQLException e) {
-            System.out.println("SQLEX" + e);
-
-        }
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-        try (Statement statement = util.getConnection().createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS USERS");
-        } catch (SQLException e) {
-            System.out.println("SQLEX" + e);
-        }
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement preparedStatement = util.getConnection().prepareStatement("INSERT INTO USERS (name, lastname,age) VALUES (?, ?, ?)")) {
-
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3,age);
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        userDaoJDBC.saveUser(name,lastname,age);
     }
     public void removeUserById(long id) {
-        try (PreparedStatement statement = util.getConnection().prepareStatement("DELETE FROM USERS WHERE user_id = ?")) {
-            statement.setLong(1,id);
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        try (Statement statement = util.getConnection().createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM hhuser");
-
-                List<User> users = new ArrayList<>();
-                User abuser = new User();
-                while (resultSet.next()) {
-
-                    abuser.setId((long) resultSet.getInt("id"));
-                    abuser.setName(String.valueOf(resultSet.getInt("name")));
-                    abuser.setLastName(String.valueOf(resultSet.getInt("lastname")));
-                    abuser.setAge((byte) resultSet.getInt("age"));
-                    users.add(abuser);
-                }
-                return users;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        userDaoJDBC.getAllUsers();
     }
     public void cleanUsersTable() {
-        try (Statement statement = util.getConnection().createStatement()) {
-            statement.executeUpdate("DELETE USERS");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+       userDaoJDBC.cleanUsersTable();
     }
 }
